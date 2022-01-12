00f3acf8c0772cd3be12116fb879dbe1e7ac0116
Your Name <you@example.com>
2022-01-12 18:23:38 +0530

    created function to draw clock

---
+++ clock.js
@@ -0,0 +1,12 @@
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+var radius = canvas.height / 2;
+ctx.translate(radius, radius);
+radius = radius * 0.90
+drawClock();
+
+function drawClock() {
+    ctx.arc(0, 0, radius, 0, 2 * Math.PI);
+    ctx.fillStyle = "yellow";
+    ctx.fill();
+}
