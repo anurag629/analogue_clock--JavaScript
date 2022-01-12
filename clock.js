a87569bdc72673c90bed236a3df92a9f6f8ded51
Your Name <you@example.com>
2022-01-12 22:00:42 +0530

    created drwFace function

--- clock.js
+++ clock.js
@@ -16,14 +16,14 @@ function drawFace(ctx, radius) {
     ctx.fillStyle = 'white';
     ctx.fill();
     grad = ctx.createRadialGradient(0, 0, radius * 0.95, 0, 0, radius * 1.05);
-    grad.addColorStop(0, 'pink');
-    grad.addColorStop(0.5, 'pink');
-    grad.addColorStop(1, 'white');
+    grad.addColorStop(0, '#333');
+    grad.addColorStop(0.5, 'white');
+    grad.addColorStop(1, '#333');
     ctx.strokeStyle = grad;
     ctx.lineWidth = radius * 0.1;
     ctx.stroke();
     ctx.beginPath();
-    ctx.arc(0, 0, radius, 0, 2 * Math.PI);
-    ctx.fillStyle = 'pink';
+    ctx.arc(0, 0, radius * 0.1, 0, 2 * Math.PI);
+    ctx.fillStyle = '#333';
     ctx.fill();
 }
